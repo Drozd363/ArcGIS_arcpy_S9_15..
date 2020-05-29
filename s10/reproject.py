5fe69327d635f0bd36477d13802780e73c296a01
dragon <drozzd3@gmil.com>
2020-05-30 01:15:22 +0300

    Generated code for redesigning the coordinate system. Independent work 10.

---
+++ s10/reproject.py
@@ -0,0 +1,19 @@
+import arcpy
+arcpy.env.overwriteOutput = True
+
+arcpy.env.workspace = r"E:\programin\semestr2\samrob\S10_GIS_FILE"
+input = "CountyLines.shp"
+
+inputSR = arcpy.Describe(input).SpatialReference
+inputSRname = inputSR.name
+arcpy.AddMessage("Coordinate systems will be changed to {}".format(inputSRname))
+listfc = arcpy.ListFeatureClasses()
+for fclas in listfc:
+    clasSR = arcpy.Describe(fclas).SpatialReference
+    clasSRname = clasSR.name
+    if clasSRname != inputSRname:
+        output = fclas[:-4] + "_projected.shp"
+        arcpy.Project_management(fclas, output, inputSR)
+        arcpy.AddMessage("The coordinate system file '{}' has been changed to {}".format(fclas, str(inputSRname)))
+    else:
+        arcpy.AddMessage("The coordinate system of the file '{}' does not need to be changed".format(fclas))
