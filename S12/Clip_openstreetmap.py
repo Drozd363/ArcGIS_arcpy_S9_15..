d2dca5e0db978c436ae30f7583c424db0af843e0
dragon <drozzd3@gmil.com>
2020-05-31 19:46:24 +0300

    s12. Update code for instrument. Created instrument.

--- S12/Clip_openstreetmap.py
+++ S12/Clip_openstreetmap.py
@@ -1,14 +1,18 @@
 import arcpy
 arcpy.env.overwriteOutput = True
-folderforgdb = r"E:\programin\semestr2\samrob\s12_GIS_FILE"
-datapoints = r"E:\programin\semestr2\samrob\s12_GIS_FILE\Programming_in_GIS_2020_L7_s12\OSMpoints.shp"
-clipzon = r"E:\programin\semestr2\samrob\s12_GIS_FILE\Programming_in_GIS_2020_L7_s12\CentralAmerica.shp"
-nameGDB = 'salvador'
+folderforgdb = arcpy.GetParameterAsText(0)
+datapoints = arcpy.GetParameterAsText(1)
+clipzon = arcpy.GetParameterAsText(2)
+nameGDB = arcpy.GetParameterAsText(3)
+#folderforgdb = r"E:\programin\semestr2\samrob\s12_GIS_FILE"
+#datapoints = r"E:\programin\semestr2\samrob\s12_GIS_FILE\Programming_in_GIS_2020_L7_s12\OSMpoints.shp"
+#clipzon = r"E:\programin\semestr2\samrob\s12_GIS_FILE\Programming_in_GIS_2020_L7_s12\CentralAmerica.shp"
+#nameGDB = 'salvador'
 arcpy.CreateFileGDB_management(folderforgdb, nameGDB + '.gdb')
 arcpy.AddMessage('Created new File GDB: {}.gdb'.format(nameGDB))
 arcpy.env.workspace = folderforgdb + "\\" + nameGDB + '.gdb'
 amenities = ['school', 'hospital', 'place_of_worship']
-country = 'El Salvador'
+country = arcpy.GetParameterAsText(4)
 arcpy.MakeFeatureLayer_management(clipzon, 'zoneclip', '"NAME" = ' + "'"+country + "'")
 arcpy.Clip_analysis(datapoints, 'zoneclip', 'clipshp')
 arcpy.AddMessage('Objects are cut for a given area ({})'.format(country))
