7b42a82f2ec2254a6e1b56426e7fdada799fb2a3
dragon <drozzd3@gmil.com>
2020-06-12 02:26:53 +0300

    s14. Bug fixes. A field is created and height data is recorded

--- s14/s14_rec_sites.py
+++ s14/s14_rec_sites.py
@@ -9,6 +9,7 @@ newFields = 'HEIGHT'
 # created result file
 arcpy.CopyFeatures_management(inputFc, resultFile)
 arcpy.AddMessage("A new file has been created with changes")
+
 # check coordinate systems for coincidence
 if arcpy.Describe(resultFile).spatialReference.name == arcpy.Describe(rasterElevation).spatialReference.name:
     arcpy.AddMessage("Coordinate systems coincide")
@@ -16,13 +17,21 @@ else:
     proj = arcpy.Describe(rasterElevation).spatialReference.name
     arcpy.Project_management(resultFile, resultFile, proj)
     arcpy.AddMessage("The coordinate systems did not match. Reprojected")
+
 # determine the value of heights in the specified coordinates
 height = []
 with arcpy.da.SearchCursor(resultFile, 'SHAPE@XY') as cursor:
     for row in cursor:
         evel = arcpy.GetCellValue_management(rasterElevation, str(row[0][0])+' '+str(row[0][1]))
-        height.append(evel)
+        height.append(evel.getOutput(0))
 arcpy.AddMessage("The values of heights by coordinates are determined")
 
-
-
+# new Field
+arcpy.AddField_management(resultFile, newFields, "SHORT")
+with arcpy.da.UpdateCursor(resultFile, newFields) as cursor:
+    i = 0
+    for row in cursor:
+        row[0] = height[i]
+        cursor.updateRow(row)
+        i += 1
+arcpy.AddMessage("Created {} field and height data is recorded".format(newFields))
