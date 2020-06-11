03841a3a930f4efea02f9491f0fd24830f27d6fe
dragon <drozzd3@gmil.com>
2020-06-12 00:01:20 +0300

    s14. Coordinate systems are checked for coincidence. The values​of raster heights by coordinates are determined.

---
+++ s14/s14_rec_sites.py
@@ -0,0 +1,33 @@
+import arcpy
+arcpy.env.overwriteOutput = True
+
+inputFc = r"E:\programin\semestr2\samrob\s14_GIS_FILE\Programming_in_GIS_2020_L9_s14\rec_sites.shp"
+rasterElevation = r"E:\programin\semestr2\samrob\s14_GIS_FILE\Programming_in_GIS_2020_L9_s14\elevation"
+resultFile = r"E:\programin\semestr2\samrob\s14_GIS_FILE\Programming_in_GIS_2020_L9_s14\rec_sites1.shp"
+newFields = 'HEIGHT'
+if newFields == '#' or not newFields:
+    newFields = 'HEIGHT'
+# Changes occur in the input or a new file is created. Asked by the user
+if resultFile == "#" or not resultFile:
+    resultFile = inputFc
+    arcpy.AddMessage("Changes occur in the input file")
+else:
+    arcpy.CopyFeatures_management(inputFc, resultFile)
+    arcpy.AddMessage("A new file has been created with changes")
+# check coordinate systems for coincidence
+if arcpy.Describe(resultFile).spatialReference.name == arcpy.Describe(rasterElevation).spatialReference.name:
+    arcpy.AddMessage("Coordinate systems coincide")
+else:
+    proj = arcpy.Describe(rasterElevation).spatialReference.name
+    arcpy.Project_management(resultFile, resultFile, proj)
+    arcpy.AddMessage("The coordinate systems did not match. Reprojected")
+# determine the value of heights in the specified coordinates
+height = []
+with arcpy.da.SearchCursor(resultFile, 'SHAPE@XY') as cursor:
+    for row in cursor:
+        evel = arcpy.GetCellValue_management(rasterElevation, str(row[0][0])+' '+str(row[0][1]))
+        height.append(evel)
+arcpy.AddMessage("The values of heights by coordinates are determined")
+
+
+
